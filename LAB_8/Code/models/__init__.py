12c262c9bb0c9cd5055d8c5c5f821c674d796657
Данилова Анастасия <nastdan07@mail.ru>
2025-12-05 12:46:53 +0300

    Add files via upload

---
+++ LAB_8/Code/models/__init__.py
@@ -0,0 +1,14 @@
+# Импорт класса Author из модуля author текущего пакета
+from .author import Author
+
+# Импорт класса App из модуля app текущего пакета
+from .app import App
+
+# Импорт класса Currency из модуля currency текущего пакета
+from .currency import Currency
+
+# Импорт класса User из модуля user текущего пакета
+from .user import User
+
+# Импорт класса UserCurrency из модуля user_currency текущего пакета
+from .user_currency import UserCurrency
